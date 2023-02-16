cb2b800ae5fe8e4a469e6967ef67bc5e7446b827
Rita <msritasanchez0@gmail.com>
2023-02-16 06:34:17 -0800

    101 quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 /**
- * main -a c program that prints followed by a new line to the standard error
+ * main - Entry point
  *
- * Return: 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\- Dora Korpar, 2015-10-19\n", 61);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+
 	return (1);
 }
