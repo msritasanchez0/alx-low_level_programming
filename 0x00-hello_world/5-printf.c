e9d1f01f8ced689bca2cacd690bf385ef607fbb4
Rita <msritasanchez0@gmail.com>
2023-02-16 02:39:58 -0800

    Printif c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
-*main -A programme that prints a line using the printf function
-*Return: 0(success)
-*/
+ *main -A programme that prints a line using the printf function
+ *Return: 0(success)
+ */
+
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n")
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+	return (0);
 }
