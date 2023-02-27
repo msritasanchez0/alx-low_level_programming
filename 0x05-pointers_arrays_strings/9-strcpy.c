75b437f7989ae6e69de29a1c0a54374e9777aa68
Rita <msritasanchez0@gmail.com>
2023-02-27 06:15:19 -0800

    copy string

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -10,10 +10,10 @@ char *_strcpy(char *dest, char *src)
 {
 	int len = 0;
 
-	while (*(src + len) != '\0')
+	while (src[len])
 	{
-		*(dest + len) = *(src + len);
+		dest[len] = src[len];
+		len++;
 	}
-	*(dest + len) = '\0';
 	return (dest);
 }
