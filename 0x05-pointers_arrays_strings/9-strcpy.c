278398f1f287f65a8560e2d29adec56033a4b3cd
Rita <msritasanchez0@gmail.com>
2023-03-01 13:27:14 -0800

    copy stings

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -6,15 +6,16 @@
  *
  * Return: String
  */
-char *_strcpy(char *dest, const char *src)
+char *_strcpy(char *dest, char *src)
 {
-	int index = 0;
+	char *start = dest;
 
-	while (src[index])
+	while (*src != '\0')
 	{
-		dest[index] = src[index];
-		index++;
+		*dest = *src;
+		dest++;
+		src++;
 	}
-
-	return (dest);
+	*dest = '\0';
+	return (start);
 }
