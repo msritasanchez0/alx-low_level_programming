f39f9f964098d2875c2ab542bd5fa5b20b056b3d
Rita <msritasanchez0@gmail.com>
2023-03-08 04:03:27 -0800

    prime number

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -11,7 +11,7 @@ int is_prime_number(int n)
 {
 	if (n <= 1)
 		return (0);
-	return (actual_prime(n, n-1));
+	return (actual_prime(n, n - 1));
 }
 /**
  * actual_prime - calculates if a number is prime recursively
