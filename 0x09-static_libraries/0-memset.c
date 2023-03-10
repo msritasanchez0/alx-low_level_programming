b5cd4c81e7bc7518ab5d7f60d01ff084f227aab4
Rita <msritasanchez0@gmail.com>
2023-03-10 05:09:53 -0800

    static library

--- 0x09-static_libraries/0-memset.c
+++ 0x09-static_libraries/0-memset.c
@@ -3,6 +3,9 @@
  * _memset - fills memory with a constant byte
  *
  * @s: pointer to memory area to fill
+ * @n: the number of bytes to fill
+ * @b: constant byte
+ *
  * Return: a pointer to the memory area @s
  */
 
