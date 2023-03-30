3633217c066e006b56828a937d65fc639e48617e
Rita <msritasanchez0@gmail.com>
2023-03-30 09:53:12 -0700

    Updates

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -8,8 +8,6 @@ void firstfunc(void) __attribute__ ((constructor));
  */
 void first(void)
 {
-	char *m;
-
-	m = "You're beat! and yet, you must allow,\nI bore my house upon my back!\n";
-	printf("%s", m);
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
