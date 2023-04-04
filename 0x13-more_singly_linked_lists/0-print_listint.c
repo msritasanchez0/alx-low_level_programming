27d8db8a02283402eb6afa69c391fcfdb2e4c74a
Rita <msritasanchez0@gmail.com>
2023-04-04 03:41:22 -0700

    Updates print list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,4 +1,6 @@
 #include "lists.h"
+#include <stdio.h>
+
 /**
  * print_listint - prints all the elements of a linked list.
  * @h: linkec list to print
@@ -8,6 +10,9 @@
 size_t print_listint(const listint_t *h)
 {
 	size_t nodes = 0;
+
+	if (!h)
+		return (0);
 
 	while (h)
 	{
