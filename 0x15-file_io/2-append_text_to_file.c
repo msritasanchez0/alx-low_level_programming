a2a71f53c6aed0157a06880215cdf83c4784177e
Rita <msritasanchez0@gmail.com>
2023-04-10 10:25:32 -0700

    append text

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * append_text_to_file - appends text at the end of the file
+ * @filename: pointer to file to create
+ * @text_conent: content file
+ *
+ * Return: 1 (success, -1 (fail)
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd, ret, len;
+
+
+	if (filename == NULL)
+		return (-1);
+
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
+
+
+	if (text_content == NULL)
+	{
+		close(fd);
+		return (1);
+	}
+	
+	len = strlen(text_content);
+	ret = write(fd, text_content, len);
+	if (ret == -1 || ret < len)
+	{
+		close(fd);
+		return (-1);
+	}
+
+	close(fd);
+
+	return (1);
+}
